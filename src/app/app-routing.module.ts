4a1f16e4319e696b570e6b4ef1bca2a49dbabfbc
Nico <nicolasjaiquel@gmail.com>
2023-10-23 17:00:38 -0500

    Cargue Inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { NotFoundComponent } from './web/pages/not-found/not-found.component';
+import { CustomPreloadService } from './services/custom-preload.service';
+import { AdminGuard } from './guards/admin.guard';
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: ()=> import('./web/web.module').then(m=> m.WebModule),
+    data: {
+      preload: true
+    },
+  },
+  {
+    path: 'cms',
+    loadChildren: ()=> import('./cms/cms.module').then(m=> m.CmsModule),
+    data: {
+      preload: false
+    },
+    canActivate: [AdminGuard]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: CustomPreloadService
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './services/users.service';
+import { TokenService } from './services/token.service';
+import { AuthService } from './services/auth.service';
+import { Auth } from './models/auth.model';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-store';
+export class AppComponent implements OnInit {
+  constructor(private tokenService: TokenService,private userService: UsersService, private authService: AuthService, private router: Router) { }
+  ngOnInit(): void {
+    const token= this.tokenService.getToken();
+    if(token){
+      this.authService.profile().
+        subscribe();
+    }
+  }
+  createUser() {
+    this.userService.createUsers({ name: 'Nicolas', email: 'nicoadmin@gmail.com', password: 'door123', role: 'admin' }).subscribe(data=> console.log(data));
+  }
+  login(){
+    this.authService.login('nicoadmin@gmail.com', 'door123').subscribe(data=> {
+      console.log(data);
+    });
+  }
+
+
 }
